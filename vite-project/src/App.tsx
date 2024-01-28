a125137aebc491af2a927de39e0ea9ba88b7a1e5
Seth Lamancusa <sethlamancusa@gmail.com>
2024-01-28 18:02:27 -0500

    Attempt at React/Flask integration

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TextBoxComponent from './components/TextBox';
+
+function App() {
+  return (
+    <div className="App">
+      <TextBoxComponent />
+    </div>
+  );
+}
+
+export default App;
