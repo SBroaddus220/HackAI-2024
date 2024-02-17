9832deb872c64eca586e550f4036b453edb729b7
J-AnealPack <J.anealpack@gmail.com>
2024-02-17 17:33:05 -0500

    Merge remote-tracking branch 'origin' into Joshua-dev

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -12,6 +12,7 @@ function App() {
         <TextBoxComponent />
       </MessagesProvider>
     </div>
+    
   );
 }
 
--- vite-project/src/contexts/MessagesContext.tsx
+++ vite-project/src/contexts/MessagesContext.tsx
@@ -37,4 +37,5 @@ export const MessagesProvider = ({ children }: MessagesProviderProps) => {
             {children}
         </MessagesContext.Provider>
     );
+    
 };
