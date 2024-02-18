30521a1a79e61d061764c71c9893061bc56f057d
Seth Lamancusa <sethlamancusa@gmail.com>
2024-02-17 22:25:35 -0500

    Bruh

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@
 Flask app for the ChromaDB retrieval process.
 """
 
+from pprint import pprint
 from flask import Flask, request, jsonify
 from flask_cors import CORS  # Import CORS
 import datetime
@@ -35,29 +36,42 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 if not OPENAI_API_KEY:
     raise ValueError("OPENAI_API_KEY environment variable not set")
 
+
 # **********
 def init_chromadb():
     global db
+    global db_spec
+    global db_reqs
     documents_txt_dir = Path("./documents/txt")
     documents_pdf_dir = Path("./documents/pdf")
     docs = load_txt_documents(documents_txt_dir)
     docs.extend(load_pdf_documents(documents_pdf_dir))
+    docs_reqs = load_txt_documents(documents_txt_dir)
+    docs_spec = load_pdf_documents(documents_pdf_dir)
+
+    db_reqs = store_documents(docs_reqs)
+    db_spec = store_documents(docs_spec)
     db = store_documents(docs)
     logger.info("ChromaDB initialized with documents.")
 
+
 # Call the function to initialize ChromaDB at the start of the app
 init_chromadb()
+
 
 # **********
 @app.route("/")
 def hello_world():
     return "Hello, World!"
 
+
 @app.route("/submit-text", methods=["POST"])
 def submit_text():
     data = request.json
+    pprint(f"request.json: {data}")
     text = data["text"]
-    
+    context = data["context"]
+
     # ****
     # User message
     current_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -66,22 +80,29 @@ def submit_text():
         "datetime": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         "role": "user",
         "message": text,
+        "context": context,
     }
     messages.append(user_message)
-    
+
     # ****
     # Generate response
-    response = query_with_retrieval(text, db, OPENAI_API_KEY)  # Notice db is used directly
+    if context == "reqs":
+        response = query_with_retrieval(text, db_reqs, OPENAI_API_KEY)
+    elif context == "spec":
+        response = query_with_retrieval(text, db_spec, OPENAI_API_KEY)
     response_message = {
         "datetime": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         "role": "user",
         "message": response,
+        "context": context,
     }
     messages.append(response_message)
-    
+
     print(text)
     print(response)
-    return jsonify({"message": "Text received successfully", "response": response_message})
+    return jsonify(
+        {"message": "Text received successfully", "response": response_message}
+    )
 
 
 @app.route("/get-messages", methods=["GET"])
@@ -93,12 +114,11 @@ def get_messages():
 # **********
 if __name__ == "__main__":
     import logging.config
+
     logging.disable(logging.DEBUG)
     logging.config.dictConfig(LOGGER_CONFIG)
-    
+
     # Load environment variables
     load_dotenv()
-    
-    app.run(debug=True, port=5000)
-    
 
+    app.run(debug=True, port=5000)
